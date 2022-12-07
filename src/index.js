e480b13885ef829017476387c436d4c15b0bf611
Onkur Lal <onkur24lal@gmail.com>
2022-12-07 06:46:15 -0500

    updated render message

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ class Board extends React.Component {
     const winner = calculateWinner(this.state.squares);
     let status;
     if (winner) {
-      status = "Winner" + winner;
+      status = "Winner " + winner;
     } else {
       status = "Next player: " + (this.state.xIsNext ? "X" : "O");
     }
